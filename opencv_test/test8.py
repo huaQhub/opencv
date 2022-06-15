4f1bb175c398f0714dc75192bdd7da53fb57af9e
DESKTOP-1MK89NV\lcl <mahuatengmeima@outlook.com>
2022-06-15 17:06:41 +0800

    学习使用摄像头分析

--- opencv_test/test8.py
+++ opencv_test/test8.py
@@ -2,13 +2,13 @@
 import numpy as np
 import cv2 as cv
 
-cap = cv.VideoCapture('202206151435.mp4')
+cap = cv.VideoCapture(0)#0为电脑内置摄像头
 
 fgbg = cv.createBackgroundSubtractorMOG2()
 
 while(1):
     ret, frame = cap.read()
-
+    frame = cv.flip(frame,1)
     fgmask = fgbg.apply(frame)
 
     cv.imshow('frame',fgmask)
--- opencv_test/test9.py
+++ opencv_test/test9.py
@@ -2,14 +2,14 @@
 import numpy as np
 import cv2 as cv
 
-cap = cv.VideoCapture('202206151435.mp4')
+cap = cv.VideoCapture(0)
 
 kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(3,3))
 fgbg = cv.bgsegm.createBackgroundSubtractorGMG()
 
 while(1):
     ret, frame = cap.read()
-
+    frame = cv.flip(frame,1)
     fgmask = fgbg.apply(frame)
     fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
 
